6d55c20e6a5a66c7711d3a6f1e3550af7ada4944
Nathan Reiter <nathan.reiter@hotmail.com>
2018-06-22 21:31:33 -0300

    atualizações do compactar.

--- bin/src/Geral/Compactador.java
+++ bin/src/Geral/Compactador.java
@@ -1,20 +1,63 @@
 package Geral;
 
 import Arvore_Binaria.ArvoreBinaria;
+import Arvore_Binaria.NoArvoreBinaria;
 
 public class Compactador {
-	
-	public void executar(String pathOrigem, String pathDestino) {
-		String string = "bom esse bom-bom"; // teste
 
-		String[] vetorLetras = string.split("");
-		ArvoreBinaria[] vetorArvores = new ArvoreBinaria[vetorLetras.length];
-		
-		for (String letra : vetorLetras) {
-			if () {
-				
+	// teste
+	protected String teste = "bom esse bom-bom";
+
+	private ArvoreBinaria<Letra>[] vetorArvores;
+	private int indiceVetorArvores = 0;
+
+	public void executar(String pathOrigem, String pathDestino) {
+
+		String[] vetorStringLetras = teste.split("");
+		Letra[] vetorLetras = preparaVetorLetras(vetorStringLetras);
+		preparaVetorArvores(vetorLetras);
+
+	}
+
+	private Letra[] preparaVetorLetras(String[] vetorStringLetras) {
+		Letra[] vetorLetras = new Letra[vetorStringLetras.length];
+		boolean repetido = false;
+
+		for (String strLetra : vetorStringLetras) {
+			for (Letra objLetra : vetorLetras) {
+				if (objLetra.getLetra().equals(strLetra)) {
+					repetido = true;
+				}
+			}
+
+			if (!repetido) {
+				vetorLetras[indiceVetorArvores] = new Letra(strLetra, buscarPeso(strLetra, vetorStringLetras));
 			}
 		}
 
+		return vetorLetras;
+	}
+
+	private void preparaVetorArvores(Letra[] vetorLetras) {
+		vetorArvores = new ArvoreBinaria[vetorLetras.length];
+
+		for (Letra letra : vetorLetras) {
+			ArvoreBinaria<Letra> novaArvore = new ArvoreBinaria<>();
+			novaArvore.setRaiz(new NoArvoreBinaria<Letra>(letra));
+			vetorArvores[indiceVetorArvores] = novaArvore;
+			indiceVetorArvores++;
+		}
+	}
+
+	private int buscarPeso(String strLetra, String[] vetorStringLetras) {
+		int cont = 0;
+
+		for (String str : vetorStringLetras) {
+			if (strLetra.equals(str)) {
+				cont++;
+			}
+		}
+
+		return cont;
 	}
 }
---
+++ bin/src/Geral/Letra.java
@@ -0,0 +1,33 @@
+package Geral;
+
+public class Letra {
+
+	private String letra;
+	private int peso;
+
+	public Letra() {
+
+	}
+
+	public Letra(String letra, int peso) {
+		this.letra = letra;
+		this.peso = peso;
+	}
+
+	public String getLetra() {
+		return letra;
+	}
+
+	public void setLetra(String letra) {
+		this.letra = letra;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+}
